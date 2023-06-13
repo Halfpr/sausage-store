a4bd2d80adf19e9c78cb65b7ef129479bc96ce2e
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-06-13 20:20:48 +0000

    Update deployback.sh

--- backend/deployback.sh
+++ backend/deployback.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
+wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O YandexInternalRootCA.crt
+sudo keytool -importcert \
+             -file YandexInternalRootCA.crt \
+             -alias yandex \
+             -cacerts \
+             -storepass changeit \
+             -noprompt
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 sudo rm -f /home/student/sausage-store.jar||true
