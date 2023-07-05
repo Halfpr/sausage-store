87029a4ce9ffee40a9f65652789fa807394c7619
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-07-05 14:55:58 +0000

    Update deployback.sh

--- backend/deployback.sh
+++ backend/deployback.sh
@@ -14,13 +14,16 @@ EOF
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend:latest
-docker stop sausage-backend || true
-docker rm sausage-backend || true
-set -e
-docker run -d --name sausage-backend \
-    --network=sausage_network \
-    --restart always \
-    --pull always \
-    --env-file .env \
-    gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend 
+docker rm -f sausage-backend || true
+#docker stop sausage-backend || true && docker rm sausage-backend || true
+docker-compose pull sausage-backend
+docker-compose up -d backend
+
+#set -e
+#docker run -d --name sausage-backend \
+#    --network=sausage_network \
+#    --restart always \
+##    --pull always \
+#    --env-file .env \
+#    gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend 
 
