6297e250e1d564e41f58edff8367d4478fa8c149
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-07-06 09:38:08 +0000

    Update deploybackreport.sh

--- backend-report/deploybackreport.sh
+++ backend-report/deploybackreport.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
-# cat > .env <<EOF
-# REPORT_PATH=/log/reports
-# SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
-# CI_REGISTRY_USER=${CI_REGISTRY_USER}
-# CI_REGISTRY_PASSWORD=${CI_REGISTRY_PASSWORD}
-# CI_REGISTRY=${CI_REGISTRY}
-# VAULT_TOKEN=${VAULT_TOKEN}
-# EOF
+cat > .env <<EOF
+SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
+CI_REGISTRY_USER=${CI_REGISTRY_USER}
+CI_REGISTRY_PASSWORD=${CI_REGISTRY_PASSWORD}
+CI_REGISTRY=${CI_REGISTRY}
+DB=$(SPRING_DATA_MONGODB_URI)
+EOF
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend-report:latest
