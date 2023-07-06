326c576c37e187f4407208cff27423059e8c416a
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-07-06 09:52:43 +0000

    Update deployfront.sh

--- frontend/deployfront.sh
+++ frontend/deployfront.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set +e
-cat > .env <<EOF
-CI_REGISTRY_USER=${CI_REGISTRY_USER}
-CI_REGISTRY_PASSWORD=${CI_REGISTRY_PASSWORD}
-CI_REGISTRY=${CI_REGISTRY}
+#set +e
+#cat > .env <<EOF
+#CI_REGISTRY_USER=${CI_REGISTRY_USER}
+#CI_REGISTRY_PASSWORD=${CI_REGISTRY_PASSWORD}
+#CI_REGISTRY=${CI_REGISTRY}
 
-EOF
+#EOF
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-frontend:latest
