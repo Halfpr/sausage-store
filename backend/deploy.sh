6e55b6978f51aa3885baa8ac4d0bed286658f0e3
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-07-12 13:38:50 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-frontend:latest
 docker rm -f sausage-frontend || true
 docker-compose pull sausage-frontend
 docker-compose up -d frontend
 
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker network create -d bridge sausage_network || true
+
 docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend:latest
 docker rm -f sausage-backend || true
 #docker stop sausage-backend || true && docker rm sausage-backend || true
