489741faab85e81f099a7b1795a49b5607adc7ed
std-015-31 std-015-31 <std-015-31@praktikum-services.ru>
2023-07-13 15:56:29 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker network create -d bridge sausage_network || true
+#docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+#docker network create -d bridge sausage_network || true
 
-if [[ $(docker inspect -f '{{.State.Running}}' backend-blue) = true ]]; then
-  docker stop backend-green || true
-  docker rm backend-green || true
-  docker-compose pull backend-green || true
-  docker-compose up -d backend-green || true
-  while [[ $(docker inspect -f '{{.State.Health.Status}}' backend-green) != "unhealthy" ]]; do
-    echo "bayubay"
-    sleep 10
-  done
-  docker stop backend-blue || true
-  docker rm -f backend-blue || true
+#if [[ $(docker inspect -f '{{.State.Running}}' backend-blue) = true ]]; then
+#  docker stop backend-green || true
+#  docker rm backend-green || true
+#  docker-compose pull backend-green || true
+#  docker-compose up -d backend-green || true
+#  while [[ $(docker inspect -f '{{.State.Health.Status}}' backend-green) != "unhealthy" ]]; do
+#    echo "bayubay"
+#    sleep 10
+#  done
+#  docker stop backend-blue || true
+#  docker rm -f backend-blue || true
 
-elif [[ $(docker inspect -f '{{.State.Running}}' backend-green) = true ]]; then
-  docker stop backend-blue || true
-  docker rm backend-blue || true
-  docker-compose pull backend-blue || true
-  docker-compose up -d backend-blue || true
-  while [[ $(docker inspect -f '{{.State.Health.Status}}' backend-blue) != "unhealthy" ]]; do
-    echo "bayubay"
-    sleep 10
-  done
-  docker stop backend-green || true
-  docker rm -f backend-green || true
-fi
+#elif [[ $(docker inspect -f '{{.State.Running}}' backend-green) = true ]]; then
+#  docker stop backend-blue || true
+#  docker rm backend-blue || true
+#  docker-compose pull backend-blue || true
+#  docker-compose up -d backend-blue || true
+#  while [[ $(docker inspect -f '{{.State.Health.Status}}' backend-blue) != "unhealthy" ]]; do
+#    echo "bayubay"
+#    sleep 10
+#  done
+#  docker stop backend-green || true
+#  docker rm -f backend-green || true
+#fi
 
 
 
@@ -40,11 +40,17 @@ fi
 #docker-compose up -d frontend
 
 
-#docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend:latest
-#docker rm -f backend-blue || true
-#docker stop sausage-backend || true && docker rm sausage-backend || true
-#docker-compose pull backend-blue
-#docker-compose up -d backend-blue
+docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend:latest
+docker rm -f backend-blue || true
+docker stop sausage-backend || true && docker rm sausage-backend || true
+docker-compose pull backend-blue
+docker-compose up -d backend-blue
+
+
+docker rm -f backend-blue-proverka || true
+docker stop sausage-backend || true && docker rm sausage-backend || true
+docker-compose pull backend-blue-proverka
+docker-compose up -d backend-blue-proverka
 
 #docker pull gitlab.praktikum-services.ru:5050/std-015-31/sausage-store/sausage-backend:latest
 #docker rm -f backend-green || true
